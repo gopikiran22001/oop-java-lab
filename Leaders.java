4261dd150a699ff0616c257a39a96a769984d4cd
gopikiran22001 <gopikiran22001@yahoo.com>
2024-07-31 18:46:38 +0530

    Update Leaders.java

--- Leaders.java
+++ Leaders.java
@@ -2,12 +2,9 @@ public class Leaders
 {
    public static void main(String args[])
    {
-    int i=0;
     int a[]={1,9,4,5,7,8,2,6,3,0};
-    for (int j : a) {
-      i++;
-
-    }for(int k=0;k<i;k++)
+      int i=a.length;
+    for(int k=0;k<i;k++)
     {
       int temp=0;
       for(int e=k+1;e<i;e++)
