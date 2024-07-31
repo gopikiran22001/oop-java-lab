ad7b1d1c59ba1af88f7f1730324b37141150ddfb
gopikiran22001 <gopikiran22001@yahoo.com>
2024-07-31 11:09:45 +0530

    Add files via upload

---
+++ Root.java
@@ -0,0 +1,27 @@
+public class Root {
+    public static void main(String[] args) {
+        double x=Double.parseDouble(args[0]);
+        double y=Double.parseDouble(args[1]);   
+        double z =Double.parseDouble(args[2]);
+        double disc=Math.sqrt(y*y-(4*x*z));
+        double root1=(-y+disc)/2*x;
+        double root2=(-y-disc)/2*x;
+        if(disc>0)
+        {
+            System.out.println("The roots are Real and Distinct");
+            System.out.println("The roots are: "+root1+" and "+root2);
+        }
+        else if(disc==0)
+        {
+            System.out.println("The roots are real and equal");
+            System.out.println("The roots are: "+root1+" and "+root2);
+        }
+        else
+        {
+            System.out.println("The equation has Complex roots Which is not possible for this specific equation");
+        }
+
+
+    }
+    
+}
