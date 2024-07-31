48d8c5da8746c9584df48d8b6f540ac9c5f0bbd4
gopikiran22001 <gopikiran22001@yahoo.com>
2024-07-31 18:20:01 +0530

    Update Setbit.java

--- Setbit.java
+++ Setbit.java
@@ -1,13 +1,11 @@
 public class Setbit {
     public static void main(String[] args) {
         int x=Integer.parseInt(args[0]);
-        int y,z=0;
-        while(x!=0)
+        int z=0;
+        while(x>0)
         {
-            y=x%2;
-            z+=y;
-            x=x/2;
-            x=(int)x;
+            z+=(x&1);
+            x=x>>1;
         }
         System.out.println("Number of set bits:"+z);
     }
