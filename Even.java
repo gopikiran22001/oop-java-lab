474859248b0fea17d4dc536c2fd5a119cf8baf27
gopikiran22001 <gopikiran22001@yahoo.com>
2024-08-03 19:10:37 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -1,20 +1,26 @@
 public class Even {
     public static void main(String[] args) {
-        int a[]={1,9,15,5,7,8,2,6,3,4};
-        int i=a.length;
-    for(int k=0;k<i;k++) {
-        for(int j=0;j<i-1;j++) {
-            if(a[j+1]%2==0)
-            {
-                int temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-            }
-    }
-    }
-for(int x=0;x<i;x++)
+    
+int[] a={3,2,6,4,5,9,8,7,4,52};
+int[] new_array=new int[a.length];
+int even_index=0,odd_index=a.length-1;
+for(int x=0;x<a.length;x++)
 {
-    System.out.print(a[x]+" ");
+    if(a[x]%2==0)
+    {
+        new_array[even_index]=a[x];
+        even_index++;
+    }
+    else
+    {
+        new_array[odd_index]=a[x];
+        odd_index--;
+    }
 }
+for(int i=0;i<a.length;i++)
+{
+    System.out.print(new_array[i]+" ");
 }
+    }
+    
 }
