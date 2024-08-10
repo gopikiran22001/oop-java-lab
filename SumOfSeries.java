a9b9194c9664f3d6f0192d2f546ac3a10a0177ae
gopikiran22001 <gopikiran22001@yahoo.com>
2024-08-10 11:57:11 +0530

    commit

---
+++ SumOfSeries.java
@@ -0,0 +1,26 @@
+public class SumOfseries
+{
+        static int sum(int x)
+    {
+        if(x<=0)
+        {
+            return 0;
+        }
+        else if(x==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return x+sum(x-1);
+        }
+    }
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.print("Enter a Number:");
+       int x=sc.nextInt();
+       int y=sum(x);
+       System.out.println("Sum from 1 to "+x+" is:"+y);
+       sc.close();
+    }
+}
