7aaf6aca4818ef22a46c342524843af63c532ec8
gopikiran22001 <gopikiran22001@yahoo.com>
2024-08-10 16:15:09 +0530

    Update Root.java

--- Root.java
+++ Root.java
@@ -4,17 +4,18 @@ public class Root {
         double y=Double.parseDouble(args[1]);   
         double z =Double.parseDouble(args[2]);
         double disc=Math.sqrt(y*y-(4*x*z));
-        double root1=(-y+disc)/2*x;
-        double root2=(-y-disc)/2*x;
         if(disc>0)
         {
+            double root1=(-y+disc)/2*x;
+            double root2=(-y-disc)/2*x;
             System.out.println("The roots are Real and Distinct");
             System.out.println("The roots are: "+root1+" and "+root2);
         }
         else if(disc==0)
         {
+            double root=-y/2*x;
             System.out.println("The roots are real and equal");
-            System.out.println("The roots are: "+root1+" and "+root2);
+            System.out.println("The roots are: "+root);
         }
         else
         {
