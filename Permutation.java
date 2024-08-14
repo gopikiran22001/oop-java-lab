6b29485f55495f8d160f10f9507365f46831edda
gopikiran22001 <gopikiran22001@yahoo.com>
2024-08-14 10:10:50 +0530

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 public class Permutation
 {
-    public static void main(String)
+	static  void per(String str,String ans)
+	{
+		if(str.length()==0)
+		{
+			System.out.println(ans+" ");
+			return;
+		}
+		for(int i=0; i<str.length(); i++)
+		{
+			char ch=str.charAt(i);
+			String ros=str.substring(0,i)+str.substring(i+1);
+			per(ros,ans+ch);
+		}
+	}
+	public static void main(String[] args)
+	{
+	    System.out.println("Enter a String:");
+	    Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		per(str," ");
+		sc.close();
+	}
 }
