97e63a64cb1b901f28720dbf0b49fa95fb60f0b4
gopikiran22001 <gopikiran22001@yahoo.com>
2024-10-26 12:27:11 +0530

    Create Common.java

---
+++ Common.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Common {
+
+    public static Set<Integer> findCom(List<Set<Integer>> sets) {
+        Set<Integer> commonElements = new HashSet<>(sets.get(0));
+        for (int i = 1; i < sets.size(); i++) {
+            commonElements.retainAll(sets.get(i));
+        }
+        return commonElements;
+    }
+
+    public static void main(String[] args) {
+        List<Set<Integer>> sets = new ArrayList<>();
+        sets.add(Set.of(1, 2, 3));
+        sets.add(Set.of(2, 3, 4));
+        sets.add(Set.of(3,5,6));
+
+        System.out.println("Common elements in all sets: " + findCom(sets));
+    }
+}
